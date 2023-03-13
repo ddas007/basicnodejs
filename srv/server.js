98b29d45571062955deaaf0dd3d9ab26e41d6385
Debashish Das <debashishdas.kol007@gmail.com>
2023-03-13 11:26:52 +0000

    Basic NodeJs with standalone route and local xsuaa service binding

---
+++ srv/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const passport = require("passport");
+const xsenv = require("@sap/xsenv");
+const JWTStrategy = require("@sap/xssec").JWTStrategy;
+const services = xsenv.getServices({ uaa:"basicnodejs-xsuaa" });  // XSUAA service
+
+const app = express();
+
+passport.use(new JWTStrategy(services.uaa));
+app.use(passport.initialize());
+app.use(passport.authenticate("JWT", { session: false }));
+
+app.get("/", function (req, res, next) {
+  res.send("Welcome User from local: " + req.user.name.givenName);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, function () {
+  console.log("Basic NodeJs listening on port " + port);
+});
